45838d5c07a3b2a419428a5b2f52f4d06f22d413
prod1992 <levon1.grigoryan@gmail.com>
2018-07-03 19:31:57 +0400

    proxy webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,20 +1,29 @@
+const pkg = require('./package');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config');
+const proxyMiddleware = require('http-proxy-middleware');
 
 module.exports = merge(webpackConfig, {
   devServer: {
     open: true,
     port: 3000,
     headers: {
-      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'DELETE, HEAD, GET, OPTIONS, POST, PUT',
       'Access-Control-Allow-Headers': 'Content-Type, Content-Range, Content-Disposition, Content-Description'
     },
-    proxy: {
-      '*': {
-        target: 'http://localhost:8888',
-      },
+    before: function (app) {
+      app.use('/parentalguidance/api', proxyMiddleware({
+        target: pkg.config.API_PROXY,
+        changeOrigin: true,
+        pathRewrite: {
+          '/parentalguidance/api' : '/api'
+        }
+      }));
+      app.use('/parentalguidance', proxyMiddleware({
+        target: 'http://localhost:8888'
+      }));
     }
   },
   devtool: 'inline-source-map'
