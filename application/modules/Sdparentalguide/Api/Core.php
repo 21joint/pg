c15b7a1e9f7ee75d94dbbddd0abefaea68f150f8
Goran Jovanov <goran.j@extfox.com>
2018-07-04 17:07:47 +0200

    My Info-family

--- application/modules/Sdparentalguide/Api/Core.php
+++ application/modules/Sdparentalguide/Api/Core.php
@@ -546,5 +546,47 @@ class Sdparentalguide_Api_Core extends Core_Api_Abstract{
 
         if (!empty($imageUrl))
             return $imageUrl;
-    }    
+    }   
+    
+
+
+    public function getDateTime($datefromtable) {
+
+        $datetime1 = new DateTime($datefromtable);
+        $datetime2 = new DateTime(date('Y/m/d'));
+        $interval = $datetime1->diff($datetime2);
+       
+
+        $years = $interval->y;
+
+        if($years>0) {
+            $months = $interval->m + ($interval->y*12);
+        }else {
+            $months = $interval->m;
+        }
+        
+        $mouthss = $months . ' Months';
+
+        if(( $months >= 0) && ( $months <= 1)){
+            $baby = 'NewBorn';   
+        }elseif(( $months >= 1) && ( $months <= 11)){
+            $baby = 'Baby';
+        }elseif(( $months >= 12) && ( $months <= 23)){
+            $baby = 'Toddler';
+        }elseif( $months >= 24 &&  $months <= 47 ){
+            $baby = 'Preschool';
+        }elseif( $months >= 48 &&  $months <= 121 ){
+            $baby = 'School-Age';
+        }elseif( $months > 112){
+            $baby = 'Teen';
+        }else {
+            $baby = '';
+        }
+
+        $users = array();
+
+        array_push($users,['baby' => $baby, 'mouth' => $mouthss ]);
+
+        return $users;
+    }
 }
