724970f40583c1c3c09ea9cb99d2aaf2115ef489
Goran Jovanov <goran.j@extfox.com>
2018-07-12 16:30:00 +0200

    For Gender and Age Range

--- application/modules/Sdparentalguide/Plugin/Signup/Family.php
+++ application/modules/Sdparentalguide/Plugin/Signup/Family.php
@@ -21,7 +21,7 @@ class Sdparentalguide_Plugin_Signup_Family extends Core_Plugin_FormSequence_Abst
 
   public function onSubmit(Zend_Controller_Request_Abstract $request)
   {
-    
+
     // Form was valid
     $skip = $request->getParam("skip");
     // do this if the form value for "skip" was not set
@@ -39,10 +39,7 @@ class Sdparentalguide_Plugin_Signup_Family extends Core_Plugin_FormSequence_Abst
   
   public function onProcess()
   {
-
-    echo "<Pre>";
-    print_r( $this->getSession()->data );
-    exit;
+   
     
     // In this case, the step was placed before the account step.
     // Register a hook to this method for onUserCreateAfter
@@ -54,21 +51,68 @@ class Sdparentalguide_Plugin_Signup_Family extends Core_Plugin_FormSequence_Abst
       return;
     }
     $user = $this->_registry->user;
-    
+ 
     $data = $this->getSession()->data;
-    if(empty($data['members']) || count($data['members']) <= 0){
-        return;
+
+    
+    // if(empty($data['members']) || count($data['members']) <= 0){
+    //     return;
+    // }
+    
+
+
+    $gender = $data['profile_gender'];
+    $age = $data['profile_age_range'];
+
+    if(empty($gender)){
+      $gender = 3;
     }
-    $members = $data['members'];
-    $catTable = Engine_Api::_()->getDbtable('familyMembers', 'sdparentalguide');
-    $familyMembers = $catTable->fetchAll($catTable->select()->where('family_member_id IN (?)',$members));
-    if(count($familyMembers) <= 0){
-        return;
-    }
-    foreach($familyMembers as $familyMember){        
-        $familyMember->owner_id = $user->getIdentity();
-        $familyMember->save();        
-    }
+
+    // Save values for gender and age
+    $user->gg_gender = $gender;
+    $user->gg_age_range = $age;
+    
+    
+  
+    //Meta for Gender
+    $fieldsMeta = Engine_Api::_()->fields()->getTable('user', 'meta');
+    $select = $fieldsMeta->select()
+                ->where('type = ?' ,'sex')
+                ->orwhere('type = ?', 'age_range');
+    
+    $rowMetaGender = $fieldsMeta->fetchAll($select);
+
+    $ageId = $rowMetaGender[0]->field_id;
+    $genderId = $rowMetaGender[1]->field_id;
+    
+    
+      $db = Engine_Db_Table::getDefaultAdapter();
+      $db->beginTransaction();
+
+    try {
+
+        // Fields table for gender
+        $table = Engine_Api::_()->fields()->getTable('user', 'values');
+        $valuesGender = $table->createRow();
+        $valuesGender->field_id = $genderId;
+        $valuesGender->item_id = $user->getIdentity();
+        $valuesGender->value = $gender;
+        $valuesGender->save();
+  
+        // Fields table for Age
+        $valueAge = $table->createRow();
+        $valueAge->field_id = $ageId;
+        $valueAge->item_id =$user->getIdentity();
+        $valueAge->value = $age; 
+        $valueAge->save();
+
+
+        // User table gender
+       
+    } catch (Exception $ex) {
+      $db->rollBack();
+      throw $ex;
+     }
     
   }
 
