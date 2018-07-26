2ad033b0671aab1ef61780f3aa049e174d0ec8b3
Dusan Jovanovski <info@extfox.com>
2018-07-26 21:38:38 +0200

    loader, hook fix

--- application/modules/Sitemailtemplates/Loader.php
+++ application/modules/Sitemailtemplates/Loader.php
@@ -17,6 +17,10 @@ class Sitemailtemplates_Loader extends Engine_Loader {
   var $_loader;
 
   public static function hook() {
+
+    echo 'Hello Core_Api_Mail Hook';
+    exit;
+
     if (self::$_hooked) {
       return;
     }
