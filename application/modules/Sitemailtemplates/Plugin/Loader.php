c5dc3298eef2a0dddd0d9073fafbe1cd95981fb9
Dusan Jovanovski <info@extfox.com>
2018-07-26 21:39:55 +0200

    Loader Path

--- application/modules/Sitemailtemplates/Plugin/Loader.php
+++ application/modules/Sitemailtemplates/Plugin/Loader.php
@@ -13,6 +13,7 @@
 class Sitemailtemplates_Plugin_Loader extends Zend_Controller_Plugin_Abstract {
 
   public function preDispatch(Zend_Controller_Request_Abstract $request) {
+    echo 'wow for our request';
     $loader = Engine_Loader::getInstance();
     if (get_class($loader) == 'Engine_Loader') {
       Sitemailtemplates_Loader::hook();
