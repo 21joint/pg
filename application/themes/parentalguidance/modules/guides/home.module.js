57751ea17572139df064ae607d85e873b149981e
prod1992 <levon1.grigoryan@gmail.com>
2018-08-10 12:57:29 +0400

    - guides home module

---
+++ application/themes/parentalguidance/modules/guides/home.module.js
@@ -0,0 +1,20 @@
+import {getGuides} from '../../services/api.service';
+
+
+(function ($) {
+
+  getGuides({
+    container: '#guidesGrid',
+    type: 'guide'
+  }, function (res) {
+    $.each(res.cards, function (i, card) {
+      $('#guidesGrid').append(card.$html);
+      let _cEl = card.$html.find('.card');
+      _cEl.addClass('card-loading');
+      setTimeout(function () {
+        _cEl.removeClass('card-loading');
+      }, i * 100);
+    })
+  });
+
+})(jQuery);
