5cfb3285d3b02282fab7286a9d6d16d59cb2d43b
prod1992 <levon1.grigoryan@gmail.com>
2018-08-15 11:38:56 +0400

    mrege

---
+++ application/themes/parentalguidance/modules/community/leaderboard.module.js
@@ -0,0 +1,21 @@
+import '../../components/leader/leader.scss';
+import {getLeaders} from '../../services/api.service';
+import {renderLeader} from "../../components/leader/leader";
+
+(function ($) {
+  getLeaders({
+    container: '#featuredLeaderboard',
+    type: 'leader'
+  }, function (leaders) {
+    $.each(leaders, function (i, leader) {
+      let _leaderHtml = renderLeader(leader, {type: 'leader'});
+
+      $('#communityLeaders').append(_leaderHtml);
+      let _cEl = $(_leaderHtml).find('.leader');
+      _cEl.addClass('member-loading');
+      setTimeout(function () {
+        _cEl.removeClass('member-loading');
+      }, i * 100);
+    });
+  });
+})(jQuery);
